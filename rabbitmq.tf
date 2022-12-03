ec27376611466db24703e0118284631d796aebe2
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-12-03 11:53:10 +0530

    updating ./terraform/tf-module-rabbitmq

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -1,10 +1,32 @@
 resource "aws_spot_instance_request" "instance" {
-  ami           = data.aws_ami.ami.image_id
-  spot_price    = data.aws_ec2_spot_price.spot_price.spot_price
-  instance_type = var.INSTANCE_TYPE
-  wait_for_fulfillment = true
+  ami                    = data.aws_ami.ami.image_id
+  spot_price             = data.aws_ec2_spot_price.spot_price.spot_price
+  instance_type          = var.INSTANCE_TYPE
+  wait_for_fulfillment   = true
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id              = var.PRIVATE_SUBNET_IDS[0]
+  iam_instance_profile   = aws_iam_instance_profile.allow-secret-manager-read-access.name
   tags = {
     Name = local.TAG_PREFIX
   }
 }
 
+resource "aws_ec2_tag" "main" {
+  resource_id = aws_spot_instance_request.instance.spot_instance_id
+  key         = "Name"
+  value       = local.TAG_PREFIX
+}
+
+resource "null_resource" "cluster" {
+
+  provisioner "remote-exec" {
+    connection {
+      user = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
+      pass = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
+      host = aws_spot_instance_request.instance.private_ip
+    }
+    inline = [
+      "ansible-pull -U https://github.com/phani-devops-practice/TRN-roboshop-ansilble.git roboshop.yml -e HOST=localhost -e ROLE=rabbitmq -e ENV=${var.ENV}"
+    ]
+  }
+}
--- vars.tf
+++ vars.tf
@@ -6,6 +6,7 @@ variable "PRIVATE_SUBNET_IDS" {}
 variable "ALLOW_SG_CIDR" {}
 variable "VPC_ID" {}
 variable "PORT" {}
+variable "WORKSTATION_IP" {}
 
 
 
