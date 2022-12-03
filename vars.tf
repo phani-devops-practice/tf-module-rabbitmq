8f78cb73e140293e70a5fd8d002d21428fcbc59e
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-12-03 09:45:39 +0530

    updating ./terraform/tf-module-rabbitmq

---
+++ vars.tf
@@ -0,0 +1,16 @@
+// RABBITMQ
+variable "ENV" {}
+variable "PROJECT" {}
+variable "INSTANCE_TYPE" {}
+variable "PRIVATE_SUBNET_IDS" {}
+variable "ALLOW_SG_CIDR" {}
+variable "VPC_ID" {}
+variable "PORT" {}
+
+
+
+
+
+
+
+
